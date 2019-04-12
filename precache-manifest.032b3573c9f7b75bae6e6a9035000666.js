b4ccd1ab1d5a3dd905d1d82f899ec0e2504a7ceb
Damian Stasiak <radom.geodetaszukapracy@gmail.com>
2019-04-12 10:04:50 +0200

    Updates

---
+++ precache-manifest.032b3573c9f7b75bae6e6a9035000666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b714b3e93ac184ff4f35",
+    "url": "/react-countries/static/js/runtime~main.b714b3e9.js"
+  },
+  {
+    "revision": "4354d329bfc455b69a40",
+    "url": "/react-countries/static/js/main.4354d329.chunk.js"
+  },
+  {
+    "revision": "f7b6ee0e68b7f33f924e",
+    "url": "/react-countries/static/js/2.f7b6ee0e.chunk.js"
+  },
+  {
+    "revision": "4354d329bfc455b69a40",
+    "url": "/react-countries/static/css/main.3f3aae69.chunk.css"
+  },
+  {
+    "revision": "e9c0d13df303956958c2405ad8ca7c46",
+    "url": "/react-countries/index.html"
+  }
+];
